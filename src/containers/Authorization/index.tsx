14c6c35bdb1c5ee2157cdade8214e19bb6e00ed3
YarynaBas1234 <jaryna.bas@gmail.com>
2021-12-14 11:20:31 +0200

    use useCallBack

--- src/containers/Authorization/index.tsx
+++ src/containers/Authorization/index.tsx
@@ -1,39 +1,28 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import { handleRegisterAction } from '../../store/auth';
+import { IHandlerRegistrationAction } from '../../store/auth/types';
 
 import { Authorization } from './Authorization';
-import { IOnRegisterAction } from '.';
-
-export * from './SignInForm';
-export * from './Authorization';
-export * from './Navigation';
-export * from './types';
-
-export enum TabConst {
-  SignIn = 'SIGN_IN',
-  SignUp = 'SIGN_UP',
-};
+import { TabConst } from './types';
 
 export const AuthorizationContainer = () => {
-  const dispatch = useDispatch();
   const [activeTab, setActiveTab] = React.useState<TabConst>(TabConst.SignIn);
 
-  const switchOnSignInTab = () => {
+  const switchOnSignInTab = React.useCallback(() => {
     setActiveTab(TabConst.SignIn);
-  };
+  }, []);
 
-  const onRegisterAction: IOnRegisterAction = (userName, password) => {
-    dispatch(handleRegisterAction(userName, password));
-  };
+  const onRegisterAction: IHandlerRegistrationAction = React.useCallback(({userName, password}) => {
+    handleRegisterAction({userName, password});
+  }, []);
 
   return (
     <Authorization
       setActiveTab={setActiveTab}
       activeTab={activeTab}
-      switchOnSignInTab={switchOnSignInTab}
       onRegisterAction={onRegisterAction}
+      switchOnSignInTab={switchOnSignInTab}
     />
   );
 };
