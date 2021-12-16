b9adaa9d0ca369010ba4aff8d61240f99166dc64
YarynaBas1234 <jaryna.bas@gmail.com>
2021-12-16 21:00:35 +0200

    Add logout button

--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { styled } from '../../styles';
-import { H5 } from '../../components';
+import { H5, ButtonText } from '../../components';
 import logo from '../../images/logo.jpg';
 
 const HeaderWrapper = styled.div`
@@ -23,17 +24,43 @@ const Logo = styled.div`
   border-radius: 50%;
 `;
 
+const RightNav = styled.div`
+  display: flex;
+`;
+
+const LogoutButton = styled.div`
+  min-width: 96px;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 0;
+  cursor: pointer;
+
+  :active {
+    font-weight: bold;
+  }
+`;
+
+const LogoutText = styled(ButtonText)`
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+`;
+
 type IHeaderProps = {
   currentUser: string | null;
 };
 
 export const Header: React.FC<IHeaderProps> = (props) => {
   const { currentUser } = props;
+  const { t } = useTranslation();
 
   return (
     <HeaderWrapper>
       <Logo />
-      <H5>{currentUser}</H5>
+      <RightNav>
+        <H5>Welcome, {currentUser}</H5>
+        <LogoutButton>
+          <LogoutText onClick={() => console.log('ok')}>{t('LOGOUT')}</LogoutText>
+        </LogoutButton>
+      </RightNav>
     </HeaderWrapper>
   );
 };
