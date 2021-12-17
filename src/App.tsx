93048a252666643d36d86ce8b4bfecad966e00a7
YarynaBas1234 <jaryna.bas@gmail.com>
2021-12-17 12:12:50 +0200

    Create Wrapper component for Root component

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,23 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Root } from 'containers';
 import { queryClient } from 'services';
 import store from 'store';
-import { theme, ThemeProvider, GlobalStyles } from 'styles';
+import { theme, ThemeProvider, GlobalStyles, styled } from 'styles';
+
+const WrapperRoot = styled.div`
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+`;
 
 const App = () => (
-  <Provider store={ store }>
-    <ThemeProvider theme={ theme }>
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
       <Router>
         <QueryClientProvider client={queryClient}>
           <GlobalStyles />
-          <Root/>
+          <WrapperRoot>
+            <Root />
+          </WrapperRoot>
         </QueryClientProvider>
       </Router>
     </ThemeProvider>
