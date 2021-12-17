e888e5810192e00c46bf4de9e6df0f5751e49552
YarynaBas1234 <jaryna.bas@gmail.com>
2021-12-17 12:33:55 +0200

    Create globalStyles for background image

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { styled } from 'styles';
+
 export const GlobalStyles = createGlobalStyle`
   * {
       margin: 0;
       padding: 0;
       box-sizing: border-box;
   }
+`;
+
+export const BackgroundImageStyles = styled.div`
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
