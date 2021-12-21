981a3ea278535335ddaf0d057dfd904dd9f5c350
YarynaBas1234 <jaryna.bas@gmail.com>
2021-12-21 10:38:02 +0200

    Refactor imports

--- src/store/users/handlers.ts
+++ src/store/users/handlers.ts
@@ -1,8 +1,7 @@
-import usersAction from './slice';
-
 import { IDispatchAction } from '../../types';
 
 import { getUsersApi } from './api';
+import usersAction from './slice';
 
 export const handleGetUsersAction: IDispatchAction = async (dispatch) => {
   const users = await getUsersApi();
